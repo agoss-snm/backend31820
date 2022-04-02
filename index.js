2bfc4a71f6155da7a50240d2aaf03b029e39d447
Agostina Romero <agostinaromontamatt@gmail.com>
2022-04-02 16:51:37 -0300

    cambios

--- index.js
+++ index.js
@@ -1,129 +1,62 @@
-const fs = require("fs");
+const express = require('express');
+const fs = require('fs')
 
-const datos = { name: 'Queso', price: 2150}
-const newDatos = { name: 'Papas', price: '36546'}
-
-
-const writeFileAsync = async (arr) => {
-  try{
-    await fs.promises.writeFile(
-    "./productos.txt",
-    JSON.stringify(arr, null, 2),
-    "utf-8"
-  )}
-  catch (err){
-    throw new Error('Error de lectura!')
-  }
-};
+const app = express();
+const port = process.env.PORT || 8080;
 
 
-const readFileAsync = async (arr) => {
-  try{
-    let file = await fs.promises.readFile("./productos.txt", "utf-8");
-    return file;
-  }
-  catch (err){
-    throw new Error('Error de lectura!')
-  }
-};
+// Rutas
+app.get('/', (req, res) => {
+    res.send(`<h1>Mi servidor express</h1>`)
+})
 
 
-class Contenedor {
-  constructor() {
-    this.producto = [];
-  }
-
-  // Metodo Save(Object)
-  async save(product) {
-    // Leo el archivo
-    let fileExits = await readFileAsync();
-
-    // Si el archivo ya contiene productos, le asigno un id correlativo y lo sumo al array de productos ya definidos.
-    if (fileExits && fileExits.length >= 0) {
-      let dataFile = JSON.parse(fileExits);
-      product.id = dataFile.length + 1;
-      dataFile.push(product);
-      this.producto = dataFile;
-      writeFileAsync(this.producto);
-
-    } 
-    else {
-      // Si el archivo esta vacio, le asigno un id y lo agrego. 
-      product.id = 1;
-      this.producto.push(product);
-      writeFileAsync(this.producto);
+// Leo el archivo
+const readFileAsync = async (path) =>{
+    try{
+        let file = await fs.promises.readFile(path, 'utf-8');
+        return file;
     }
-  }
-
-  // Metodo getById(Number)
-  async getById(id, position){
-    let fileExits = await readFileAsync();
-
-    // Si el archivo existe, lo parseo y realizo la busqueda del id
-    if (fileExits) {
-      let dataFile = JSON.parse(fileExits);
-      const found = dataFile.find(element => element.id == id)
-      // console.log(dataFile.indexOf(found))
-
-      if(!found){
-        return console.log('null')
-      }
-      if (position){
-        return dataFile.indexOf(found)
-      }else{
-        // retorno el objeto encontrado
-        return console.log(found)
-      } 
+    catch(err){
+        throw new Error('Error de lectura!..')
     }
-  }
-
-  // Metodo getAll(Number)
-  async getAll(){
-    let fileExits = await readFileAsync();
-    
-    if (fileExits) {
-      let dataFile = JSON.parse(fileExits);
-      this.producto.push(dataFile);
-      console.log(this.producto)
-    }
-  }
-
-  // Metodo deleteById(Number)
-  async deleteById(id){
-   const positionItem = await this.getById(id, true)
-   console.log(positionItem)
-
-    let fileExits = await readFileAsync();
-
-    // Si el archivo existe y el id buscado existe, se elimina del archivo.
-    if (fileExits && positionItem >= 0) {
-      let dataFile = JSON.parse(fileExits);
-      const deleteItem = dataFile.splice(positionItem, 1)
-      
-      console.log(dataFile)
-
-      // Actualizo el archivo
-      writeFileAsync(dataFile);
-    }
-    else
-      console.log('No se pudo eliminar!')
-  }
-
-
-  // Metodo deleteAll(Number)
-  async deleteAll(){ 
-      writeFileAsync(this.producto);
-  }
-
 }
 
+class Contenedor {
+    constructor(){
+        this.producto = []
+    }
+
+    async getAll(path){
+        let data = await readFileAsync(path);
+
+        if (data){
+            let dataFile = JSON.parse(data);
+        
+        // Devolver array con todos los productos
+        app.get('/products', (req, res) => {
+            res.json(dataFile)
+        })
+
+        // Devolver producto al azar
+        app.get('/getramdomproducts', (req, res) => {
+            let productRandom = dataFile[Math.floor(Math.random() * dataFile.length)]
+            res.json(productRandom)
+        })
+
+        }
+    }
+}
+
+let ListProducts = new Contenedor();
+ListProducts.getAll("./productos.txt")
 
 
-let c = new Contenedor();
 
-// Ejecucion de metodos
-c.save(newDatos);
-c.getById(3)
-c.getAll()
-//c.deleteById(6)
-//c.deleteAll()
+
+
+// listen port
+app.listen(port, () => {
+    console.log('Server run on port ' + port)
+})
+
